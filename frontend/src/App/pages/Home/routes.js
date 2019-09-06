aa215f1eb275137d790e434dd8abd33239dea4bb
ERTuringLee <ehdspt@naver.com>
2019-09-06 17:17:10 +0900

    🔨:라우트수정

--- frontend/src/App/pages/Home/routes.js
+++ frontend/src/App/pages/Home/routes.js
@@ -1,16 +1,10 @@
 import Main from "./pages/Main";
-import SignIn from "./pages/SignIn"
 
 const routes = [
   {
     path: "/",
     exact: true,
     component: Main
-  },
-  {
-    path: "/login",
-    exact: true,
-    component: SignIn
   }
 ];
 
